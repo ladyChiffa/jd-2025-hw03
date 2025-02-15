33415f43c788160cc054af12b31323e7f5adc076
Оксана Миленина <42136772+ladyChiffa@users.noreply.github.com>
2025-02-15 18:47:43 +0300

    Delete IllegalStateException.java

--- IllegalStateException.java
+++
@@ -1,5 +0,0 @@
-public class IllegalStateException extends RuntimeException {
-    public IllegalStateException ( String attrName ){
-        super( "Чекеру не установлен аттрибут: " + attrName );
-    }
-}
