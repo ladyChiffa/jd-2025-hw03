2154e88600f959db25de47022ce3f0410cd32f40
Оксана Миленина <42136772+ladyChiffa@users.noreply.github.com>
2025-02-15 18:47:31 +0300

    Delete IllegalArgumentException.java

--- IllegalArgumentException.java
+++
@@ -1,5 +0,0 @@
-public class IllegalArgumentException extends RuntimeException {
-    public IllegalArgumentException( int count ){
-        super( "Нельзя передавать значение <0, передали: " + count );
-    }
-}
